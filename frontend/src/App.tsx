85d621945644876acfeb9ce02012800081c2de43
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2025-01-04 17:11:47 +0530

    Initializing Frontend - A react project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+import Landing from './screens/Landing';
+import Game from './screens/Game';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
